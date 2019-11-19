750b153811371c22186a2300b33b5dfccb715400
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-11-19 14:36:03 -0500

    Add top-level export for chains

--- spec/index.ts
+++ spec/index.ts
@@ -1,15 +1,18 @@
 import ResponseTypes from './responses';
 import ValidatorTypes from './validators';
 import EventTypes from './events';
+import ChainTypes from './chains';
 
 export import Response = ResponseTypes;
 export import Validate = ValidatorTypes;
 export import Event = EventTypes;
+export import Chain = ChainTypes;
 
 export namespace ApiTypes {
   export import Response = ResponseTypes;
   export import Validate = ValidatorTypes;
   export import Event = EventTypes;
+  export import Chain = ChainTypes;
 }
 
 export default ApiTypes;
