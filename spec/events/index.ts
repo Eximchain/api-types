e12ea98d570b91a9a5790a1d72de0caa8ec0ba6e
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-11-19 12:26:39 -0500

    Move events module one level down

--- spec/events/index.ts
+++ spec/events/index.ts
@@ -3,14 +3,7 @@
 // If we didn't do this namespace here,
 // then a consumer would have to do it in order
 // to get a convenient namespace.
-import * as APIGatewayTypes from './api-gateway-event-type';
-import * as CodePipelineTypes from './codepipeline-event-type';
-import * as SQSTypes from './sqs-event-type';
-export namespace Events {
-    export import APIGateway = APIGatewayTypes;
-    export import CodePipeline = CodePipelineTypes;
-    export import SQS = SQSTypes;
-}
+import * as Events from './event-types';
 
 // These lines directly surface all of its non-default
 // exports from right here, so that users can just
@@ -18,9 +11,7 @@ export namespace Events {
 //
 // import { APIGatewayEvent } from 'api-types/spec/events'
 //
-export * from './api-gateway-event-type';
-export * from './codepipeline-event-type';
-export * from './sqs-event-type';
+export * from './event-types';
 
 // This line exports the other file's namespace by default,
 // so that consumers can grab all of its exports under one
